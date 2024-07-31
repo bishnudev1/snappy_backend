11d12c8f7f9386eb3bc31e65edb7dea0b6619e8e
Bishnudev Khutia <91979889+bishnudev1@users.noreply.github.com>
2024-07-31 12:14:07 -0700

    feat: frontend-changed-again

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://snappy-client-vghl.onrender.com",
+    origin: "https://snappyz.netlify.app",
     credentials: true,
   },
 });
