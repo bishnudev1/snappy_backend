73ebb020593efebe5699c6f5812fd04ead16ab4e
Bishnudev Khutia <91979889+bishnudev1@users.noreply.github.com>
2024-07-31 12:10:57 -0700

    feat: frontend-url-changed

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://snappy-client-vghl.onrender.com",
     credentials: true,
   },
 });
